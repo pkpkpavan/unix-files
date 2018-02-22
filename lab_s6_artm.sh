556ad243af82462571ade2f2789c868699418da6
pkpkpavan <35984266+pkpkpavan@users.noreply.github.com>
2018-02-22 10:16:41 +0530

    Update lab_s6_artm.sh

--- lab_s6_artm.sh
+++ lab_s6_artm.sh
@@ -1,23 +1,23 @@
 echo "enter two numbers"
-read n1
-read n2
+read a
+read b
 echo "enter choice \n1.add\n2.subtract\n3.multiply\n4.division\n5.modules"
 read var
 case $var in
 	1)
-		sum=`expr $n1 + $n2`
+		sum=`expr $a + $b`
 		echo "sum ="$sum;;
 	2)
-		sum=`expr $n1 - $n2`
+		sum=`expr $a - $b`
                  echo "sub ="$sum;;
 	3)
-		sum=`expr $n1 \* $n2`
+		sum=`expr $a \* $b`
                   echo "mul ="$sum;;
         4)
-		sum=`expr $n1 / $n2`
+		sum=`expr $a / $b`
 		 echo "div ="$sum;;
 	 5)
-                sum=`expr $n1 % $n2`
+                sum=`expr $a % $b`
 		echo "mod ="$sum
  esac 
 
